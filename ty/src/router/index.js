ea70798717d8c9ef6cd98bad710a24e0a94788ab
zhangwenbo <358192832@qq.com>
2019-02-09 22:19:44 +0800

    Update index.js

--- ty/src/router/index.js
+++ ty/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/components/login/login.vue'
+import Home from '@/components/page/home.vue'
+import User from '@/components/page/users/users.vue'
 
 Vue.use(Router)
 
@@ -10,6 +12,14 @@ export default new Router({
       name: 'login',
       path: '/login',
       component: Login
+    },
+    {
+      name: 'home',
+      path: '/home',
+      component: Home,
+      children: [
+        { path: '/users', component: User } // /users
+      ]
     }
   ]
 })
