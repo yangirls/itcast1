b514741173028b3ae4ff8b7d8dff998524cb666f
zhangwenbo <358192832@qq.com>
2019-02-09 23:02:08 +0800

    Update sidebar.vue

--- ty/src/layout/sidebar.vue
+++ ty/src/layout/sidebar.vue
@@ -1,66 +1,14 @@
 <template>
     <el-menu :router="true" :unique-opened="true" default-active="2" class="el-menu-vertical-demo mymenu">
-        <el-submenu index="1">
+        <el-submenu v-for="item1 in rightsList" :key="item1.id" :index="item1.path">
             <!-- 一级菜单 -->
             <template slot="title">
                 <i class="el-icon-location"></i>
-                <span>用户管理</span>
+                <span>{{item1.authName}}</span>
             </template>
-            <el-menu-item index="/users">
+            <el-menu-item v-for="item2 in item1.children" :key="item2.id" :index="'/' + item2.path">
                 <i class="el-icon-menu"></i>
-                <span>用户列表</span>
-            </el-menu-item>
-        </el-submenu>
-        <el-submenu index="2">
-            <template slot="title">
-                <i class="el-icon-location"></i>
-                <span>权限管理</span>
-            </template>
-            <el-menu-item index="/roles">
-                <i class="el-icon-menu"></i>
-                <span>角色列表</span>
-            </el-menu-item>
-            <el-menu-item index="/rights">
-                <i class="el-icon-menu"></i>
-                <span>权限列表</span>
-            </el-menu-item>
-        </el-submenu>
-        <el-submenu index="3">
-            <template slot="title">
-                <i class="el-icon-location"></i>
-                <span>商品管理</span>
-            </template>
-            <el-menu-item index="3-1">
-                <i class="el-icon-menu"></i>
-                <span>商品列表</span>
-            </el-menu-item>
-            <el-menu-item index="3-2">
-                <i class="el-icon-menu"></i>
-                <span>分类参数</span>
-            </el-menu-item>
-            <el-menu-item index="3-3">
-                <i class="el-icon-menu"></i>
-                <span>商品分类</span>
-            </el-menu-item>
-        </el-submenu>
-        <el-submenu index="4">
-            <template slot="title">
-                <i class="el-icon-location"></i>
-                <span>订单管理</span>
-            </template>
-            <el-menu-item index="4-1">
-                <i class="el-icon-menu"></i>
-                <span>订单列表</span>
-            </el-menu-item>
-        </el-submenu>
-        <el-submenu index="5">
-            <template slot="title">
-                <i class="el-icon-location"></i>
-                <span>数据统计</span>
-            </template>
-            <el-menu-item index="5-1">
-                <i class="el-icon-menu"></i>
-                <span>数据报表</span>
+                <span>{{item2.authName}}</span>
             </el-menu-item>
         </el-submenu>
     </el-menu>
@@ -68,12 +16,38 @@
 
 <script>
 export default {
-
+  data () {
+    return {
+      rightsList: []
+    }
+  },
+  methods: {
+    // 用来得到当前登录用户的所有的权限
+    async getRightsList () {
+      var res = await this.$http.request({
+        url: '/menus',
+        method: 'get',
+        headers: {
+          'Authorization': window.localStorage.getItem('token')
+        }
+      })
+      var { meta, data } = res.data
+      if (meta.status === 200) {
+        this.rightsList = data
+      } else {
+        this.$message.error(meta.msg)
+      }
+    }
+  },
+  mounted () {
+    this.getRightsList()
+  }
 }
 </script>
 
 <style>
 /* 侧边栏样式 */
+
 .mymenu {
     height: 100%
 }
