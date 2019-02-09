915d9c41a09f970b4f8c0d60ee1f4f546c4a2c83
zhangwenbo <358192832@qq.com>
2019-02-09 22:31:05 +0800

    Update index.js

--- ty/src/router/index.js
+++ ty/src/router/index.js
@@ -9,6 +9,10 @@ Vue.use(Router)
 export default new Router({
   routes: [
     {
+      path: '/',
+      redirect: '/home'
+    },
+    {
       name: 'login',
       path: '/login',
       component: Login
