dfee3e66000329e6ae5d56185f4b0a0696c44e68
zhangwenbo <358192832@qq.com>
2019-02-09 23:02:04 +0800

    Update home.vue

--- ty/src/components/page/home.vue
+++ ty/src/components/page/home.vue
@@ -26,12 +26,12 @@ export default {
   },
   beforeCreate () {
     // 1.0 验证是否： token
-    var token = window.localStorage.getItem('token')
-    if (!token) {
-      // 跳转回登录页面
-      this.$message.error('您还没有登录，请先登录')
-      this.$router.push('/login')
-    }
+    // var token = window.localStorage.getItem('token')
+    // if (!token) {
+    //   // 跳转回登录页面
+    //   this.$message.error('您还没有登录，请先登录')
+    //   this.$router.push('/login')
+    // }
   }
 }
 </script>
