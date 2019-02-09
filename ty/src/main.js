e551478d771583f1e4344603f005744afd0d69a8
zhangwenbo <358192832@qq.com>
2019-02-09 22:19:41 +0800

    Update main.js

--- ty/src/main.js
+++ ty/src/main.js
@@ -4,15 +4,16 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import Element from 'element-ui'
-import axios from 'axios'
+// import axios from 'axios'
+import myaxios from '@/assets/js/myaxios.js'
 import 'element-ui/lib/theme-chalk/index.css'
 import '@/assets/css/index.css'
 
-// 统一设置请求路径
-axios.defaults.baseURL = 'http://localhost:8888/api/private/v1'
-// 挂载到 vue 的原型中
-Vue.prototype.$http = axios
-
+// // 统一设置请求路径
+// axios.defaults.baseURL = 'http://localhost:8888/api/private/v1'
+// // 挂载到 vue 的原型中
+// Vue.prototype.$http = axios
+Vue.use(myaxios)
 Vue.use(Element)
 
 Vue.config.productionTip = false
